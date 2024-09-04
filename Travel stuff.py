ab22184e923538d43036dadab3bfaa7da36fb815
Aakash Kuila <A.Kuila@albynschool.org>
2024-09-04 08:35:42 +0100

    modified:   Travel stuff.py

--- Travel stuff.py
+++ Travel stuff.py
@@ -1,1 +1,13 @@
-travel_agent = []
+travel_agent=[]
+booking_number=[]
+dates=[]
+for index in range(3):
+    travelagent=input("Enter your travel agent ")
+    bookingnumber=int(input("Enter your booking number "))
+    date=input("Enter your date of travel ")
+    travel_agent.append(travelagent)
+    booking_number.append(bookingnumber)
+    dates.append(date)
+print(travel_agent)
+print(booking_number)
+print(dates)
