bac803404951832a5ce65e1b23233242a63c7f7e
Aakash Kuila <A.Kuila@albynschool.org>
2024-10-04 12:09:34 +0100

    git commit

---
+++ Running total task 2.py
@@ -0,0 +1,6 @@
+total = 0
+
+for index in range(5):
+    thistestscore = int(input("What percentage score did you achieve? "))
+    total += thistestscore
+    print ("Your overall score is ",total)
