8aacd00f341831308369e09890d9489e922b8df5
Aakash Kuila <A.Kuila@albynschool.org>
2024-10-09 08:50:03 +0100

    git commit

---
+++ Running Total Task 1 Monthly savings.py
@@ -0,0 +1,6 @@
+total = 0
+
+for index in range(12):
+    thismonthsavings = int(input("Enter this months savings: "))
+    total += thismonthsavings
+    print ("Your overall savings so far is",total,"pounds.")
